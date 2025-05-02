bbd173b1f3e7a70a40497832100f80bb742ff4a8
Johannes Niederhauser <johannes.niederhauser@uibk.ac.at>
2025-05-02 10:22:04 +0200

    fix results for WANDA

--- cade_experiments_other_tools.sh
+++ cade_experiments_other_tools.sh
@@ -1,55 +1,55 @@
 echo "--- Example 4 (extended) --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/diff2.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/diff2.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/diff2.trs
 echo "--- Example 5 --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/nnf.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/nnf.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/nnf.trs
 echo "--- Example 6 --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/map_inc.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/map_inc.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/map_inc.trs
 echo "--- Example 5.2 in [BJR15] --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/hofmann.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/hofmann.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/hofmann.trs
 echo "--- Example 7.1 in [BJR15] --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/brouwerord.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/brouwerord.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/brouwerord.trs
 echo "--- Example 8.19 in [BJR15] --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/applicative_treeflatten.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/applicative_treeflatten.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/applicative_treeflatten.trs
 echo "--- Example 7.1 in [JR15] --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/pnf.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/pnf.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/pnf.trs
 echo "--- Example 7.2 in [JR15] --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/sdu.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/sdu.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/sdu.trs
 echo "--- Example 7.3 in [JR15] --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/pad.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/pad.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/pad.trs
 echo "--- neutr --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/neutr.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/neutr.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/neutr.trs
 echo "--- neutrN --"
 echo "WANDA:"
-./wanda.exe --betafirst --disable=nt,dp,rem,rr,poly --silent examples/other_formats/neutrN.afsm
+./wanda.exe --betafirst -d nt,poly,dp --silent examples/other_formats/neutrN.afsm
 echo "CSI^ho:"
 ./csiho --category HO --strategy horpo -p -t examples/other_formats/neutrN.trs
